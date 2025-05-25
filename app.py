4e6482baea193c7e4dd9eaa76794fa2b2c8c6482
Chandrashekar <rcchandrashekar1999@gmail.com>
2025-05-25 12:28:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    print("Trying to render index.html from:", os.listdir("templates"))
     return render_template('index.html')
 
 if __name__ == '__main__':
